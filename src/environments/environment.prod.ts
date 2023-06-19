4ab60b81206c561d65231dd726994968d75b0b95
Matias Pereyra <matias.pereyra1@gmail.com>
2023-06-19 18:29:38 -0300

    GRI-14 commit inicial de environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const config = {
+  production: true,
+  apiUrl: 'https://api.example.com',
+  apiKey: 'your-api-key',
+  apiSecret: 'your-api-secret'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const config = {
+  production: false,
+  apiUrl: 'https://api.example.com',
+  apiKey: 'your-api-key',
+  apiSecret: 'your-api-secret'
+};
