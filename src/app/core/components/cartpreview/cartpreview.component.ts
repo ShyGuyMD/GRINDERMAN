efa27793e1e8027d09319b4533b32f02479be7b4
Vodkarellart <mfguerra.clases@gmail.com>
2023-06-18 21:43:28 -0300

    Creacion y estilo de CartPreview, creacion del componente Header.

---
+++ src/app/core/components/cartpreview/cartpreview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartpreview',
+  templateUrl: './cartpreview.component.html',
+  styleUrls: ['./cartpreview.component.css']
+})
+export class CartpreviewComponent {
+
+}
--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,3 +1,5 @@
+import { CartpreviewComponent } from "./cartpreview";
+import { HeaderComponent } from "./header";
 import { LayoutComponent } from "./layout";
 import { MenuComponent } from "./menu";
 import { SearchbarComponent } from "./searchbar";
@@ -5,9 +7,13 @@ import { SearchbarComponent } from "./searchbar";
 export * from './layout';
 export * from './menu';
 export * from './searchbar';
+export * from './cartpreview';
+export * from './header';
 
 export const components = [
     LayoutComponent,
     MenuComponent,
-    SearchbarComponent
+    SearchbarComponent,
+    CartpreviewComponent,
+    HeaderComponent
 ]
