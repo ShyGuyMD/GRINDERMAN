1043fe0f89e459d12515330eee6cc53d1af2dccb
Vodkarellart <mfguerra.clases@gmail.com>
2023-06-25 15:02:14 -0300

    Creación y funacionalidad básica de los servicios Authentication, User y Utils

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,10 +1,19 @@
 import { ApiService } from './api';
+import { AuthenticationService } from './authentication';
 import { ExceptionService } from './exception';
+import { UserService } from './user';
+import { UtilsService } from './utils';
 
 export * from './api';
 export * from './exception';
+export * from './authentication';
+export * from './utils';
+export * from './user';
 
 export const components = [
     ApiService,
-    ExceptionService
+    ExceptionService,
+    AuthenticationService,
+    UtilsService,
+    UserService
 ]
