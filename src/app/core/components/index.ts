347d88152d4e316c6b0c471b8e7a245c59b7cfeb
Vodkarellart <mfguerra.clases@gmail.com>
2023-06-25 16:16:26 -0300

    GRI-11 Aregado el blank page component

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,3 +1,4 @@
+import { BlankPageComponent } from "./blank-page";
 import { CartpreviewComponent } from "./cartpreview";
 import { HeaderComponent } from "./header";
 import { LayoutComponent } from "./layout";
@@ -5,18 +6,20 @@ import { MenuComponent } from "./menu";
 import { SearchbarComponent } from "./searchbar";
 import { UserMenuComponent } from "./user-menu";
 
+export * from './blank-page';
+export * from './cartpreview';
+export * from './header';
 export * from './layout';
 export * from './menu';
 export * from './searchbar';
-export * from './cartpreview';
-export * from './header';
 export * from './user-menu';
 
 export const components = [
+    BlankPageComponent,
+    CartpreviewComponent,
+    HeaderComponent,
     LayoutComponent,
     MenuComponent,
     SearchbarComponent,
-    CartpreviewComponent,
-    HeaderComponent,
     UserMenuComponent
 ]
