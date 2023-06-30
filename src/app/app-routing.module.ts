1c9ae86e708948870df15716b25039bf9e994c9a
Vodkarellart <mfguerra.clases@gmail.com>
2023-06-30 20:12:24 -0300

    GRI-26 Creacion de componentes y routing, catalogo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 // Import your components for routing
 import { StyleTemplateComponent } from './style-template/style-template.component';
 import { LayoutComponent } from '@core/components/layout/layout.component';
-import { BookCreateComponent, BookDetailComponent } from '@core/components/book';
+import { BookCatalogComponent, BookCreateComponent, BookDetailComponent } from '@core/components/book';
 
 const routes: Routes = [
   { path: '', redirectTo: 'style-template', pathMatch: 'full'},
@@ -16,6 +16,7 @@ const routes: Routes = [
       },
       { path: 'book-create', component: BookCreateComponent },
       { path: 'book-detail', component: BookDetailComponent },
+      { path: 'catalog', component: BookCatalogComponent },
     ] 
   },
   
--- src/app/core/components/book/index.ts
+++ src/app/core/components/book/index.ts
@@ -1,2 +1,3 @@
 export * from './book-create/book-create.component';
 export * from './book-detail/book-detail.component';
+export * from './book-catalog/book-catalog.component';
