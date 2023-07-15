0829db3f7e728396499014d150a2db22a486f02e
Matias Pereyra <matias.pereyra1@gmail.com>
2023-07-15 17:14:54 -0300

    GRI-29 - Agregado servicio Shared para comunicación entre componentes ruteados, agregada funcionalidad de búsqueda

--- src/app/core/components/searchbar/searchbar.component.ts
+++ src/app/core/components/searchbar/searchbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { WooCommerceApiService } from '@core/services';
+import { SharedService } from '@core/services/shared';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-searchbar',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent {
+
+  searchTerm = new Subject<string>();
+  isLoading = false;
+
+  constructor(
+    private _sharedService : SharedService,
+    private _wooCommerceAPIService : WooCommerceApiService) {}
+
+  ngOnInit() {
+      this.searchTerm.pipe(
+          debounceTime(300) // tiempo en ms a esperar por otro keyup antes de mandar el request
+      ).subscribe({
+          next: (v) => {
+              this.isLoading = true;
+              this.performSearch(v)
+          },
+          error: (e) => console.log('Error Debouncing: ', e)
+      });
+  }
+
+  handleSearchEvent(keyword: string) {
+      this.searchTerm.next(keyword);
+  }
+
+  performSearch(keyword: string) {
+      this._wooCommerceAPIService.getProducts(keyword).subscribe({
+        next: (v) => this._sharedService.setSearchResults(v),
+        error: (e) => console.log('Error in Search API: ', e),
+        complete: () => this.isLoading = false
+      });
+  }
 
 }
---
+++ src/app/core/services/shared/shared.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  private searchResultSubject = new BehaviorSubject<any[]>([]);
+  searchResult$ = this.searchResultSubject.asObservable();
+  
+  setSearchResults(results: any[]) {
+    this.searchResultSubject.next(results);
+  }
+
+  getSearchResults(): any[] {
+    return this.searchResultSubject.value;
+  }
+}
