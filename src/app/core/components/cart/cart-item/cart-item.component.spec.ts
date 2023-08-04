b432f42457a40fece97b9c6c5f5469dabbe6018c
Vodkarellart <mfguerra.clases@gmail.com>
2023-08-04 10:28:35 -0300

    GRI-57 Arreglo a configuración de los test unitarios de Cart-item

--- src/app/core/components/cart/cart-item/cart-item.component.spec.ts
+++ src/app/core/components/cart/cart-item/cart-item.component.spec.ts
@@ -1,21 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CartItemComponent } from './cart-item.component';
+import { CartItem } from '@core/models/cartItem';
+import { mockBook1 } from '@core/mocks/book.service.mock';
 
 describe('CartItemComponent', () => {
   let component: CartItemComponent;
   let fixture: ComponentFixture<CartItemComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  const mockCartItem: CartItem = {
+    book: mockBook1, // Provide valid book data
+    quantity: 2
+  };
+  
+  beforeEach(waitForAsync ( () => {
+   TestBed.configureTestingModule({
       declarations: [ CartItemComponent ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CartItemComponent);
     component = fixture.componentInstance;
+    component.item = mockCartItem;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
