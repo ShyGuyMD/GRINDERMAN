0f48f047536114f3a0525aa899e608f6b1edfbf3
Matias Pereyra <matias.pereyra1@gmail.com>
2023-09-10 22:33:14 -0300

    Just the api keys workaround
    (cherry picked from commit 6591ed9a362b9723554ac09f5608c4a2eed8ef92)

--- src/app/core/services/woo-commerce/woo-commerce-api.service.ts
+++ src/app/core/services/woo-commerce/woo-commerce-api.service.ts
@@ -6,154 +6,174 @@ import { ProductService } from '../product';
 import { CreateCustomerRequest } from '@core/models/request/createCustomerRequest';
 import { AuthenticationService } from '../authentication';
 import { CreateOrderRequest } from '@core/models/request/createOrderRequest';
-import {
-  CreateOrderResponse,
-  RetrieveOrderResponse,
-} from '@core/models/response/orderResponse';
-import {
-  Observable,
-  forkJoin,
-  map,
-  mergeMap,
-  take,
-  takeUntil,
-  takeWhile,
-} from 'rxjs';
-import { HttpParams } from '@angular/common/http';
+import { CreateOrderResponse, RetrieveOrderResponse } from '@core/models/response/orderResponse';
+import { Observable, forkJoin, map, take, takeWhile } from 'rxjs';
+import { HttpHeaders, HttpParams } from '@angular/common/http';
 import { Product } from '@core/models/product';
 import { ApiBodyRequest } from '@core/models/request/apiBodyRequest';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class WooCommerceApiService {
-  private baseUrl = config.baseUrl + config.wc;
-  private readonly headers = this._authService.getAuthorizationHeader();
+    private baseUrl = config.baseUrl + config.wc;
+    private headers = this._authService.getAuthorizationHeader();
+    private readonly basicAuth = btoa(`${config.apiKey}:${config.apiSecret}`);
 
-  constructor(
-    private _apiService: ApiService,
-    private _authService: AuthenticationService,
-    private _productService: ProductService
-  ) {}
+    constructor(
+        private _apiService: ApiService,
+        private _authService: AuthenticationService,
+        private _productService: ProductService
+    ) { }
 
-  public getAllProducts(): Observable<Product[]> {
-    const maxPages = 100;
-    const productObservables: Observable<Product[]>[] = [];
+    public getAllProducts(): Observable<Product[]> {
+        const maxPages = 100;
+        const productObservables: Observable<Product[]>[] = [];
 
-    for (let page = 1; page <= maxPages; page++) {
-      productObservables.push(this.getProducts(page));
+        for (let page = 1; page <= maxPages; page++) {
+            productObservables.push(this.getProducts(page));
+        }
+
+        return forkJoin(productObservables).pipe(
+            takeWhile((response) => response.length > 0), // Stop when response is empty
+            map((responses) => responses.reduce((acc, curr) => acc.concat(curr), [])), // Flatten the array of arrays
+            take(1)
+        );
     }
 
-    return forkJoin(productObservables).pipe(
-      takeWhile((response) => response.length > 0), // Stop when response is empty
-      map((responses) => responses.reduce((acc, curr) => acc.concat(curr), [])), // Flatten the array of arrays
-      take(1)
-    );
-  }
+    public getProducts(page: number): Observable<Product[]> {
+        const url = `${this.baseUrl}/products`;
+        const params = new HttpParams().set('page', page.toString());
 
-  public getProducts(page: number): Observable<Product[]> {
-    const url = `${this.baseUrl}/products`;
-    const params = new HttpParams().set('page', page.toString());
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
 
-    return this._apiService.get(url, this.headers, params);
-  }
-
-  getProductsById(productId: number) {
-    const url = `${this.baseUrl}/products/${productId}`;
-
-    return this._apiService.get(url, this.headers);
-  }
-
-  getProductsByKeyword(keyword: string, page: number = 1) {
-    const url = `${this.baseUrl}/products?search=${keyword}`;
-    const params = new HttpParams().set('page', page.toString());
-
-    return this._apiService.get(url, this.headers, params);
-  }
-
-  getProductAttributes() {
-    const url = `${this.baseUrl}/products/attributes`;
-
-    return this._apiService.get(url, this.headers);
-  }
-
-  getProductAttributeTerms(attrId: number, page: number) {
-    const url = `${this.baseUrl}/products/attributes/${attrId}/terms`;
-    const params = new HttpParams().set('page', page.toString());
-    return this._apiService.get(url, this.headers, params);
-  }
-
-  public getAllProductAttributeTerms(attrId: number): Observable<Product[]> {
-    const maxPages = 10;
-    const attrObservables: Observable<any[]>[] = [];
-
-    for (let page = 1; page <= maxPages; page++) {
-        attrObservables.push(this.getProductAttributeTerms(attrId, page));
+        return this._apiService.get(url, this.headers, params);
     }
 
-    return forkJoin(attrObservables).pipe(
-        takeWhile((responses) => responses.length > 0), // Stop when response is empty
-        map((responses) => responses.reduce((acc, curr) => acc.concat(curr), [])), // Flatten the array of arrays
-        take(1)
-    );
- }
-  postProduct(book: Book) {
-    const url = `${this.baseUrl}/products`;
+    getProductsById(productId: number) {
+        const url = `${this.baseUrl}/products/${productId}`;
 
-    const body = this._productService.mapBookToProduct(book);
-    console.log('bookproduct', body);
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
 
-    return this._apiService.post(url, body, this.headers);
-  }
-
-  postProductAttributeTerms(productId: number, termValue: string) {
-    const url = `${this.baseUrl}/products/attributes/${productId}/terms`;
-    const body = { name: termValue };
-
-    return this._apiService.post(url, body, this.headers);
-  }
-
-  putProductData(productId: number, body: any) {
-    const url = `${this.baseUrl}/products/${productId}`;
-
-    return this._apiService.put(url, body, this.headers);
-  }
-
-  public batchUpdateProducts(create: Book[], update:Book[], del: number[] = []){
-    const createProducts = create.map(book => this._productService.mapBookToProduct(book));
-    const updateProducts = update.map(book => this._productService.mapBookToProduct(book));
-
-    const url = `${this.baseUrl}/products/batch`;
-
-    const request: ApiBodyRequest = {
-      create: createProducts,
-      update: updateProducts,
-      delete: del
+        return this._apiService.get(url, this.headers);
     }
-    return this._apiService.post(url, request, this.headers);
-  }
 
-  public postCustomer(body: CreateCustomerRequest): any {
-    const url = `${this.baseUrl}/customers`;
+    getProductsByKeyword(keyword: string, page: number = 1) {
+        const url = `${this.baseUrl}/products`;
+        const params = new HttpParams()
+                            .set('search', keyword)
+                            .set('page', page.toString());
 
-    return this._apiService.post(url, body, this.headers);
-  }
+                            this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
 
-  public postOrder(body: CreateOrderRequest): Observable<CreateOrderResponse> {
-    const url = `${this.baseUrl}/orders`;
+        return this._apiService.get(url, this.headers, params);
+    }
 
-    return this._apiService.post(url, body, this.headers);
-  }
+    getProductAttributes() {
+        const url = `${this.baseUrl}/products/attributes`;
 
-  public getOrdersById(orderId: number): Observable<RetrieveOrderResponse> {
-    const url = `${this.baseUrl}/orders/${orderId}`;
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+        console.log('SEND ITTTTTTTT');
 
-    return this._apiService.get(url, this.headers);
-  }
+        return this._apiService.get(url, this.headers);
+    }
 
-  public getAllOrders(): Observable<RetrieveOrderResponse[]> {
-    const url = `${this.baseUrl}/orders`;
+    getProductAttributeTerms(attrId: number, page: number) {
+        const url = `${this.baseUrl}/products/attributes/${attrId}/terms`;
+        const params = new HttpParams().set('page', page.toString());
 
-    return this._apiService.get(url, this.headers);
-  }
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.get(url, this.headers, params);
+    }
+
+    public getAllProductAttributeTerms(attrId: number): Observable<Product[]> {
+        const maxPages = 10;
+        const attrObservables: Observable<any[]>[] = [];
+
+        for (let page = 1; page <= maxPages; page++) {
+            attrObservables.push(this.getProductAttributeTerms(attrId, page));
+        }
+
+        return forkJoin(attrObservables).pipe(
+            takeWhile((responses) => responses.length > 0), // Stop when response is empty
+            map((responses) => responses.reduce((acc, curr) => acc.concat(curr), [])), // Flatten the array of arrays
+            take(1)
+        );
+    }
+
+    postProduct(book: Book) {
+        const url = `${this.baseUrl}/products`;
+
+        const body = this._productService.mapBookToProduct(book);
+        console.log('bookproduct', body);
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.post(url, body, this.headers);
+    }
+
+    postProductAttributeTerms(productId: number, termValue: string) {
+        const url = `${this.baseUrl}/products/attributes/${productId}/terms`;
+        const body = { name: termValue };
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.post(url, body, this.headers);
+    }
+
+    putProductData(productId: number, body: any) {
+        const url = `${this.baseUrl}/products/${productId}`;
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.put(url, body, this.headers);
+    }
+
+    public batchUpdateProducts(create: Book[], update: Book[], del: number[] = []) {
+        const createProducts = create.map(book => this._productService.mapBookToProduct(book));
+        const updateProducts = update.map(book => this._productService.mapBookToProduct(book));
+
+        const url = `${this.baseUrl}/products/batch`;
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        const request: ApiBodyRequest = {
+            create: createProducts,
+            update: updateProducts,
+            delete: del
+        }
+        return this._apiService.post(url, request, this.headers);
+    }
+
+    public postCustomer(body: CreateCustomerRequest): any {
+        const url = `${this.baseUrl}/customers`;
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.post(url, body, this.headers);
+    }
+
+    public postOrder(body: CreateOrderRequest): Observable<CreateOrderResponse> {
+        const url = `${this.baseUrl}/orders`;
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.post(url, body, this.headers);
+    }
+
+    public getOrdersById(orderId: number): Observable<RetrieveOrderResponse> {
+        const url = `${this.baseUrl}/orders/${orderId}`;
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.get(url, this.headers);
+    }
+
+    public getAllOrders(): Observable<RetrieveOrderResponse[]> {
+        const url = `${this.baseUrl}/orders`;
+
+        this.headers = new HttpHeaders().set('Authorization', 'Basic ' + this.basicAuth);
+
+        return this._apiService.get(url, this.headers);
+    }
 }
