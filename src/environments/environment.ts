391dd901573a5df059a815c918b6751fe2e1dda0
Flori <90431795+Vodkarellart@users.noreply.github.com>
2023-09-20 00:01:44 -0300

    environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const config = {
+	production: false,
+    // ya no importan, pero dejarlas por las dudas
+    apiKey: 'ck_045e6094f28f8f091addcb12fe59bd62a5b49bb6',
+    apiSecret: 'cs_46e202845b570fb26953b558914a41df93c5f446',
+    // -----
+    baseUrl: 'https://localhost/wordpress/wp-json/',
+    cc: 'cocart/v2',
+    jwt: 'jwt-auth/v1',
+    wp: 'wp/v2',
+    wc: 'wc/v3',
+    //Google
+    gmApiKey: 'AIzaSyDnUWoGzlbF8GofSzrozaoExbhRhxZWkaY',
+    gmUrl: 'https://maps.googleapis.com/maps/api/js?key=',
+    //admin
+    ourUser: 'wp_admin',
+    ourPassword: 'T2Ns Fwu0 ikZK EThy BOsI zoid'
+};
