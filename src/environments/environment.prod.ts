5316d486e1d893d1a0393cd6a51eaf8b87e10d46
Matias Pereyra <matias.pereyra1@gmail.com>
2023-09-21 11:04:28 -0300

    GRI-73 - Cherry Pick cuando se tenga que hacer un deploy.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const config = {
+    production: true,
+    // ya no importan, pero dejarlas por las dudas
+    apiKey: 'ck_d9709c254bd835e89ddb7fb374e32d1f3728f06e',
+    apiSecret: 'cs_1c874a7e1e9406d13d90087eb89ca5df6c5478c5',
+    // -----
+    baseUrl: 'https://wp-dev.grinderman.store/wp-json/',
+    cc: 'cocart/v2',
+    jwt: 'jwt-auth/v1',
+    wp: 'wp/v2',
+    wc: 'wc/v3',
+    //Google
+    gmApiKey: 'AIzaSyDnUWoGzlbF8GofSzrozaoExbhRhxZWkaY',
+    gmUrl: 'https://maps.googleapis.com/maps/api/js?key='
+};
